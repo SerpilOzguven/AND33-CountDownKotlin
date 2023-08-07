485d70faf3a05bec62a648bc1862b443c4d644a4
Serpil Özgüven <serpilozguven776@gmail.com>
2023-08-07 18:22:23 +0300

    Initial commit

---
+++ app/src/main/java/com/serpilozguven/countdownkotlin/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.serpilozguven.countdownkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.serpilozguven.countdownkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Abstract Interface Inheritance
+
+        object : CountDownTimer(20000,1000) {
+            override fun onTick(p0: Long) {
+                binding.textView.text = "Left: ${p0 / 1000}"
+            }
+
+            override fun onFinish() {
+                binding.textView.text = "Left:0"
+            }
+
+        }.start()
+
+
+
+
+
+    }
+}
